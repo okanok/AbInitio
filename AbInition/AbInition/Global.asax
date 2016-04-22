9aca3757edf18727bd3abb587849e7b92d93d13e
okanok <okanok94@gmail.com>
2016-04-22 10:32:03 +0200

    web app
    ASP.NET applicatie opgezet

---
+++ AbInition/AbInition/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AbInition.MvcApplication" Language="C#" %>
---
+++ AbInition/AbInition/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AbInition.Startup))]
+namespace AbInition
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
