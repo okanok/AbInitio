14d32230a8dddb769f1509ea3dd0edfa6b6e7361
okanok <okanok94@gmail.com>
2016-04-22 11:42:58 +0200

    nieuw web app gemaakt

---
+++ Ab Initio/Ab Initio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ab_Initio.MvcApplication" Language="C#" %>
---
+++ Ab Initio/Ab Initio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ab_Initio.Startup))]
+namespace Ab_Initio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
